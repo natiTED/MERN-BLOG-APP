48afeef0ad65e2ffa54025c758febad951a84285
natiTED <123365384+natiTED@users.noreply.github.com>
2024-08-14 19:42:26 +0300

    Update Signin.jsx

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -7,7 +7,7 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
-import OAuth from "../components/OAuth";
+// import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -90,7 +90,7 @@ export default function SignIn() {
                 "Sign In"
               )}
             </Button>
-            <OAuth />
+{/*             <OAuth /> */}
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont Have an account?</span>
