20ec3b8c0ad9353e9cd9d966adf4aa5fd02ed9f1
naaty ted <natnaeltewodros9@gmail.com>
2024-08-15 19:08:07 +0300

    Deployment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import CallToAction from "../components/CallToAction";
 import { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
 
+// just to check
+
 export default function Home() {
   const [posts, setPosts] = useState([]);
 
