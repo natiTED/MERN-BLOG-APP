5fa1f5945041b2d8b40a781d8e8fcff313530d21
naaty ted <natnaeltewodros9@gmail.com>
2024-08-28 16:06:38 +0300

    last1

--- api/index.js
+++ api/index.js
@@ -28,11 +28,11 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+// app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
